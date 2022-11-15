cf35b897ce4450a4523dcb9735a80d1e2e348b2e
aydun1 <ayduno@gmail.com>
2022-11-15 11:34:08 +1000

    Fix for slashes in customer number

--- src/index.ts
+++ src/index.ts
@@ -87,8 +87,8 @@ app.get('/gp/customers', auth, (req: Request, res: Response) => {
   );
 });
 
-app.get('/gp/customers/:id', auth, (req: Request, res: Response) => {
-  getCustomer(req.params.id).then(
+app.get('/gp/customers/:id/addresses', auth, (req: Request, res: Response) => {
+  getCustomerAddresses(req.params.id).then(
     result => res.status(200).send(result)
   ).catch(
     err => {
@@ -98,8 +98,8 @@ app.get('/gp/customers/:id', auth, (req: Request, res: Response) => {
   );
 });
 
-app.get('/gp/customers/:id/addresses', auth, (req: Request, res: Response) => {
-  getCustomerAddresses(req.params.id).then(
+app.get('/gp/customers/:id(*)', auth, (req: Request, res: Response) => {
+  getCustomer(req.params.id).then(
     result => res.status(200).send(result)
   ).catch(
     err => {
