52ca6b037a831b251db0e6cdb6dd9c895c1088ed
aydun1 <ayduno@gmail.com>
2023-04-12 16:22:42 +1000

    Get order lines

--- src/services/gp.service.ts
+++ src/services/gp.service.ts
@@ -468,51 +468,13 @@ export function getOrders(branch: string, batch: string) {
   const request = new sqlRequest();
   const query =
   `
-
-  SELECT
-  RTRIM(BACHNUMB) batchNumber,
-  DOCDATE docDate,
-  ReqShipDate reqShipDate,
-  SOPTYPE sopType,
-  RTRIM(SOPNUMBE) sopNumber,
-  ORIGTYPE origType,
-  RTRIM(ORIGNUMB) origNumber,
-  RTRIM(CUSTNMBR) custNumber,
-  RTRIM(CUSTNAME) custName,
-  RTRIM(ShipToName) shipToName,
-  RTRIM(Address1) address1,
-  RTRIM(ADDRESS2) address2,
-  RTRIM(ADDRESS3) address3,
-  RTRIM(CITY) city,
-  RTRIM([STATE]) state,
-  RTRIM(ZIPCODE) postCode,
-  RTRIM(SHIPMTHD) shipMethod,
-  0 posted
+  SELECT RTRIM(BACHNUMB) batchNumber, DOCDATE docDate, ReqShipDate reqShipDate, SOPTYPE sopType, RTRIM(SOPNUMBE) sopNumber, ORIGTYPE origType, RTRIM(ORIGNUMB) origNumber, RTRIM(CUSTNMBR) custNumber, RTRIM(CUSTNAME) custName, RTRIM(ShipToName) shipToName, RTRIM(Address1) address1, RTRIM(ADDRESS2) address2, RTRIM(ADDRESS3) address3, RTRIM(CITY) city, RTRIM([STATE]) state, RTRIM(ZIPCODE) postCode, RTRIM(SHIPMTHD) shipMethod, 0 posted
   FROM SOP10100
   WHERE LOCNCODE = @locnCode
   AND (SOPTYPE = 2)
   AND ReqShipDate = @date
   UNION ALL
-  SELECT
-  RTRIM(BACHNUMB) batchNumber,
-  DOCDATE docDate,
-  ReqShipDate reqShipDate,
-  SOPTYPE sopType,
-  RTRIM(SOPNUMBE) sopNumber,
-  ORIGTYPE origType,
-  RTRIM(ORIGNUMB) origNumber,
-  RTRIM(CUSTNMBR) custNumber,
-  RTRIM(CUSTNAME) custName,
-  RTRIM(ShipToName) shipToName,
-  RTRIM(Address1) address1,
-  RTRIM(ADDRESS2) address2,
-  RTRIM(ADDRESS3) address3,
-  RTRIM(CITY) city,
-  RTRIM([STATE]) state,
-  RTRIM(ZIPCODE) postCode,
-  RTRIM(SHIPMTHD) shipMethod,
-  1 posted
-
+  SELECT RTRIM(BACHNUMB) batchNumber, DOCDATE docDate, ReqShipDate reqShipDate, SOPTYPE sopType, RTRIM(SOPNUMBE) sopNumber, ORIGTYPE origType, RTRIM(ORIGNUMB) origNumber, RTRIM(CUSTNMBR) custNumber, RTRIM(CUSTNAME) custName, RTRIM(ShipToName) shipToName, RTRIM(ADDRESS1) address1, RTRIM(ADDRESS2) address2, RTRIM(ADDRESS3) address3, RTRIM(CITY) city, RTRIM([STATE]) state, RTRIM(ZIPCODE) postCode, RTRIM(SHIPMTHD) shipMethod, 1 posted
   FROM SOP30200
   WHERE ReqShipDate = @date
   AND LOCNCODE = @locnCode
@@ -524,9 +486,30 @@ export function getOrders(branch: string, batch: string) {
   return request.input('date', VarChar(23), date).input('locnCode', VarChar(12), branch).query(query).then((_: IResult<gpRes>) => {return {orders: _.recordset}});
 }
 
-//export function getOrderLines(sopType: string, sopNumber: string) {
-//  select * from SOP10200
-//}
+export function getOrderLines(sopType: number, sopNumber: string) {
+  const request = new sqlRequest();
+  const query = `
+  SELECT SOPTYPE, SOPNUMBE, ITEMNMBR itemNmbr, ITEMDESC itemDesc, QTYPRINV * QTYBSUOM quantity, LNITMSEQ
+  FROM (
+    SELECT a.SOPTYPE, a.SOPNUMBE, b.ITEMNMBR, b.ITEMDESC, b.QTYPRINV, b.QTYBSUOM, b.LNITMSEQ
+    FROM SOP10100 a
+    LEFT JOIN SOP10200 b
+    ON a.SOPTYPE = b.SOPTYPE and a.SOPNUMBE = b.SOPNUMBE
+    UNION
+    SELECT a.SOPTYPE, a.SOPNUMBE, b.ITEMNMBR, b.ITEMDESC, b.QTYPRINV, b.QTYBSUOM, b.LNITMSEQ
+    FROM SOP30200 a
+    LEFT JOIN SOP30300 b
+    ON a.SOPTYPE = b.SOPTYPE and a.SOPNUMBE = b.SOPNUMBE
+  ) t
+  WHERE SOPTYPE = @soptype
+  AND SOPNUMBE = @sopnumber
+  AND QTYPRINV * QTYBSUOM > 0
+  ORDER BY LNITMSEQ ASC
+  `;
+  return request.input('soptype', SmallInt, sopType).input('sopnumber', Char(21), sopNumber).query(query).then((_: IResult<gpRes>) => {return {lines  : _.recordset}});
+
+
+}
 
 export function getChemicals(branch: string, itemNumber: string, type: string, order: string, orderby: string): Promise<{chemicals: CwRow[]}> {
   branch = parseBranch(branch);
