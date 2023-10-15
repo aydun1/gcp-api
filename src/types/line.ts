977bb56d37a1ae4369b013212d0c6b412f739504
aydun1 <ayduno@gmail.com>
2023-10-16 09:44:35 +1000

    Fix types

--- src/types/line.ts
+++ src/types/line.ts
@@ -3,10 +3,10 @@ export interface Line {
   poNumber: string;
   reqDate: string;
   itemDesc: string;
-  itemNumber: string;
+  ItemNmbr: string;
   lineNumber: string;
   orderQty: number;
   cancelledQty: number;
   extendedCost: number;
-  toTransfer: number;
+  ToTransfer: number;
 }
