dbf2db28af20ba39352b4d5910e73e1c99f1bf39
aydun <ayduno@gmail.com>
2024-09-25 11:02:45 +1000

    Remove debug

--- src/services/helper.service.ts
+++ src/services/helper.service.ts
@@ -3,9 +3,6 @@ import { exec } from 'shelljs';
 export async function runShellCmd(cmd: string) {
   return new Promise((resolve, reject) => {
     exec(cmd, async (code, stdout, stderr) => {
-      console.log(code);
-      console.log(stdout);
-      console.log(stderr);
       if (!code) {
         return resolve(stdout);
       }
